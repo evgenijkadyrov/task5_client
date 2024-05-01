33d31140b5b220a4159fbc57de10533859683f50
Yauheni Kadyrau <7077634@gmail.com>
2024-05-01 16:29:08 +0300

    fix: input controlled

--- src/components/errorsInput/index.tsx
+++ src/components/errorsInput/index.tsx
@@ -1,38 +1,43 @@
 import {ChangeEvent, useState} from 'react';
+import {Input, Space} from "antd";
 
 export const ErrorsInput = () => {
-    const [errors, setErrors] = useState(0);
-    const [number, setNumber] = useState(0);
-    const handleErrorsChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(event.target.value);
-        setErrors(value);
-        setNumber(value);
+
+    const [value, setValue] = useState('0');
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setValue(value);
     };
 
-    const handleNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(event.target.value);
-        setNumber(value);
-        setErrors(value);
-    };
 
     return (
         <div>
-            <label htmlFor="errors">Errors:</label>
-            <input
-                id="errors"
-                type="range"
-                min={0}
-                max={10}
-                value={errors}
-                onChange={handleErrorsChange}
-            />
-            <input
-                type="number"
-                min={0}
-                max={1000}
-                value={number}
-                onChange={handleNumberChange}
-            />
+            <label htmlFor="errors">Errors:  </label>
+            <Space size={[8, 16]}>
+
+                <Space.Compact>
+                    <Input
+                        id="errors"
+                        type="range"
+                        min={0}
+                        step={0.5}
+                        max={10}
+                        value={value >= 10 ? 10 : value}
+                        onChange={handleInputChange}
+                    />
+                </Space.Compact>
+                <Space.Compact>
+                    <Input
+                        type="number"
+                        min={0}
+                        max={1000}
+                        step={0.5}
+                        value={value}
+                        onChange={handleInputChange}
+                    />
+                </Space.Compact>
+
+            </Space>
         </div>
     );
 };
